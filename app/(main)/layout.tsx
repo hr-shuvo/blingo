1f3302d16194327af1253af924fe59c007b05476
hr-shuvo <h.r.shuvo9@gmail.com>
2024-11-07 10:04:57 +0600

    learn page wrapper

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -13,7 +13,7 @@ const MainLayout = ({children}:Props)=>{
             <MobileHeader/>
             <Sidebar className='hidden lg:flex'/>
             <main className='lg:pl-[256px] h-full pt-[50px] lg:pt-0'>
-                <div className='bg-red-500 h-full'>
+                <div className='h-full max-w-[1056px] mx-auto pt-6 '>
                     {children}
                 </div>
 
